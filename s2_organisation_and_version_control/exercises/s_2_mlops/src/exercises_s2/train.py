59cf0721574b2fb5cc4548c5aa3502d2e7532340
Kaja Hovinbøle <kajahovin@icloud.com>
2026-01-09 11:39:58 +0100

    s1 finished + starting loading dataset for s2

---
+++ s2_organisation_and_version_control/exercises/s_2_mlops/src/exercises_s2/train.py
@@ -0,0 +1,10 @@
+from exercises_s2.model import Model
+from exercises_s2.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
